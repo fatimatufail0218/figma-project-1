2ae03321174a90f277ab6466451a9b6ad20fba64
fatimatufail0218 <fatimatufail0218@gmail.com>
2026-06-17 13:25:53 +0500

    fixed again mongodb.ts and casestudy

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,27 +1,23 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URI;
 
 if (!uri) {
   throw new Error("MONGODB_URI not defined");
 }
 
-// Top level pe rakho - yahan kaam karta hai
 declare global {
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+  var _mongoClientPromise: Promise<MongoClient>;
 }
+
+let client = new MongoClient(uri);
 
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    const client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  const client = new MongoClient(uri);
-  clientPromise = client.connect();
+if (!global._mongoClientPromise) {
+  global._mongoClientPromise = client.connect();
 }
+
+clientPromise = global._mongoClientPromise;
 
 export default clientPromise;
