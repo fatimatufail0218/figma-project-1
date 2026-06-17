d58866c04c15663672e86457fb8d1b1b3607f960
fatimatufail0218 <fatimatufail0218@gmail.com>
2026-06-17 15:12:22 +0500

    changed mongodb.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,23 +1,42 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI!;
 
 if (!uri) {
   throw new Error("MONGODB_URI not defined");
 }
 
 declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
-
-let client = new MongoClient(uri);
 
 let clientPromise: Promise<MongoClient>;
 
-if (!global._mongoClientPromise) {
-  global._mongoClientPromise = client.connect();
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+      tls: true,
+      tlsAllowInvalidCertificates: false,
+    });
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+    tls: true,
+    tlsAllowInvalidCertificates: false,
+  });
+  clientPromise = client.connect();
 }
-
-clientPromise = global._mongoClientPromise;
 
 export default clientPromise;
