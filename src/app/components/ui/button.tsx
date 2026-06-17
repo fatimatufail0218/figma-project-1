70d03b5db2a307f86be524df951ca54c17cd6dc3
fatimatufail0218 <fatimatufail0218@gmail.com>
2026-06-17 12:03:25 +0500

    button modified

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button = ({text}) =>{
+const Button = ({text}: { text: string }) =>{
     return(
         <div className="bg-white font-black font-roboto font-bold border-0 rounded-4xl text-center h-auto py-2 px-6 w-fit text-[12px] md:text-[18px]">
             {text}
